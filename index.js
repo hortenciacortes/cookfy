c91bb551438b0a2b0f062bd7a6b8eea4c1e70896
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-02-06 11:41:23 -0400

    photo modal actions

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Modal = {
     openClose() {
-        document
-            .querySelector(".modal-overlay")
-            .classList.toggle("modal-active");
+        document.querySelector("#modal-sidebar-active").classList.toggle("modal-active");
     },
+    openCloseBox(){
+        document.querySelector("#modal-box-active").classList.toggle("modal-active");
+    }
 };
 
 document.getElementById("user").addEventListener("click", Modal.openClose);
-document.querySelector(".closeModal").addEventListener("click", Modal.openClose);
+document.querySelector(".closeSidebar").addEventListener("click", Modal.openClose);
 
+document.querySelector('.areaDescription').addEventListener("click", Modal. openCloseBox)
+document.querySelector(".closeBox").addEventListener("click", Modal.openCloseBox);
